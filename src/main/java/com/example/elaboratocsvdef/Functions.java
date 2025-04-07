92d09771a2eb3e55794cfdce74d47028cce802ec
longo.21132 <longo.alessandro.studente@itispaleocapa.it>
2025-04-07 12:41:44 +0200

    metodo aggiungi campo

--- src/main/java/com/example/elaboratocsvdef/Functions.java
+++ src/main/java/com/example/elaboratocsvdef/Functions.java
@@ -1,11 +1,35 @@
 package com.example.elaboratocsvdef;
 
 import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 public class Functions {
-    public static void addRecord(){
-        int min = 10;
-        int max = 20;
-        int numero = (int)(Math.random() * (max - min + 1)) + min;
+        public static void aggiungiCampo(){
+            try{
+            BufferedReader br = new BufferedReader(new FileReader("Longo.csv"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("Longo.csv"));
+            String riga;
+            boolean primaRiga = true;
+            Random random = new Random();
+
+            while ((riga = br.readLine()) != null) {
+                if (primaRiga) {
+                    riga += ",miovalore,cancellato";
+                    primaRiga = false;
+                } else {
+                    int numero = random.nextInt(11) + 10;
+                    riga += "," + numero + ",false";
+                }
+                bw.write(riga);
+                bw.newLine();
+            }
+
+            br.close();
+            bw.close();
+            System.out.println("Campo aggiunto con successo");
+        }catch (IOException e){
+                e.printStackTrace();
+            }
     }
 }
